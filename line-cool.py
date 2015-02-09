4b9b93be0bef612ec202773c2f76221fa5f7f53e
intoblack <intoblack86@gmail.com>
2015-02-09 22:56:09 +0800

    1.更改line-cool格式化输出

--- line-cool.py
+++ line-cool.py
@@ -1,18 +1,18 @@
-#coding=utf-8
+# coding=utf-8
 
 import sublime
 import sublime_plugin
 import re
 
 
-
 class LineCoolCommand(sublime_plugin.TextCommand):
 
-    #暂时定义\n三个以上为块分隔符
+    # 暂时定义\n三个以上为块分隔符
 
     __split = re.compile('\n{3,}').split
-    placeholder = "    "
+    placeholder = ""
     line_split = '\t\t'
+
     def get_cur_content(self):
         '''
         得到当前编辑view中的所有文本
@@ -23,17 +23,17 @@ class LineCoolCommand(sublime_plugin.TextCommand):
         contents = []
         content = self.get_cur_content()
         data_container = self.__split(content)
-        #得到每个块内部pieces
+        # 得到每个块内部pieces
         contents = [
             [pic for pic in chunk.split('\n')]
             for chunk in data_container
         ]
-        #获得最大块含有行数
+        # 获得最大块含有行数
         max_len = max(len(chunk) for chunk in contents)
-        #块数
+        # 块数
         chunk_size = len(contents)
         datas = []
-        for  i in range(max_len):
+        for i in range(max_len):
             items = []
             for j in range(chunk_size):
                 item = self.placeholder
@@ -45,13 +45,17 @@ class LineCoolCommand(sublime_plugin.TextCommand):
         # for items in datas:
         #     print self.line_split.join(items)
 
-
-    def create_new_file(self , contents):
-        window = sublime.active_window() #得到当前活动window
-        view = window.new_file() #创建文件view
-        edit = view.begin_edit() #开始编辑view
-        _contents = '\n'.join([self.line_split.join(items) for items in contents]) #转换为文本
-        view.insert(edit , 0 , _contents)
+    def create_new_file(self, contents):
+        window = sublime.active_window()  # 得到当前活动window
+        view = window.new_file()  # 创建文件view
+        edit = view.begin_edit()  # 开始编辑view
+        _contents = '\n'.join(
+            [
+                ''.join(['{0:<10s}'.format(item) for item in items])
+                for items in contents
+            ]
+        )  # 转换为文本
+        view.insert(edit, 0, _contents)
         view.end_edit()
 
 
